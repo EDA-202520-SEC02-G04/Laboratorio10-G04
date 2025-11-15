bddcbb2a1a2b677e754da9cfd6129f84db82640d
mateoamaya02 <m.amayap@uniandes.edu.co>
2025-11-15 13:19:42 -0500

    d

--- App/logic.py
+++ App/logic.py
@@ -133,17 +133,12 @@ def load_services(analyzer, servicesfile, stopsfile):
 
 
 def total_stops(analyzer):
-    """
-    Total de paradas de autobus en el grafo
-    """
-    # TODO: Retorne el número de vértices del grafo
+    return G.order(analyzer['connections'])
+
 
 
 def total_connections(analyzer):
-    """
-    Total de enlaces entre las paradas
-    """
-    # TODO: Retorne el número de arcos del grafo de conexiones
+    return G.size(analyzer['connections'])
 
 
 # Funciones para la medición de tiempos
