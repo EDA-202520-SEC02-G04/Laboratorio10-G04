0f4992ab91bfdb61c1c7dabf4f59b3ba0ed6a409
tomasjara2 <t.jara1@uniandes.edu.co>
2025-11-20 08:47:49 -0500

    dfs2

---
+++ DataStructures/dfs.py
@@ -0,0 +1,11 @@
+def dfs(my_graph, source):
+    pass
+
+def dfs_vertex(my_graph, vertex, visited_map):
+    pass
+
+def has_path_to(key_v, visited_map):
+    pass
+
+def path_to(key_v, visited_map):
+    pass
