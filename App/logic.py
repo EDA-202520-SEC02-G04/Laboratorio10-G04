bbcf468a8607d106dcbea7c421022ec18cedb6bb
Simon Pineda <simonpine@Simons-MacBook-Pro-4.local>
2026-01-30 07:02:17 -0500

    hola

--- App/logic.py
+++ App/logic.py
@@ -51,6 +51,8 @@ del modelo en una sola respuesta.  Esta responsabilidad
 recae sobre el controlador.
 """
 
+
+
 # ___________________________________________________
 #  Inicializacion del catalogo
 # ___________________________________________________
